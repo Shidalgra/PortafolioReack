c3d06c375591daf4cefc11b8db5a1ff65b6dde22
Shidalgra <shidalgra@gmail.com>
2025-10-14 16:50:46 -0600

    Primer commit, agrego lo que ya tenía en localmente

---
+++ porfafolio-react/run-dev.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "D:\Personal\Github Repos\PortafolioReact\PortafolioReack\porfafolio-react"
+echo Iniciando servidor de desarrollo...
+npm run dev
+pause
---
+++ porfafolio-react/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { NavBar } from './components/NavBar.jsx';
+import { Banner } from './components/Banner.jsx';
+import { Skills } from './components/Skills.jsx';
+import { Projects } from './components/Projects.jsx';
+import { Contact } from './components/Contact.jsx';
+import { Footer } from './components/Footer.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
