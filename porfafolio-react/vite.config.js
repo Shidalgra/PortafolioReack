6382b447962749f04b3a0ab43c06e2aec6315ce0
Shidalgra <shidalgra@gmail.com>
2025-10-14 16:58:54 -0600

    Para que se pueda ver desde netlify

--- porfafolio-react/vite.config.js
+++ porfafolio-react/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PortafolioReack/',
+  base: '/', // Para Netlify usar '/', para GitHub Pages usar '/PortafolioReack/'
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
